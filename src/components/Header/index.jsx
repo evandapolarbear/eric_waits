a4200eb090e0b613d0ac0a1cd57bb10dfb7878d5
Evan Waits <evan.waits@procore.com>
2019-04-30 18:32:07 -0700

    copy edits and header mobile css

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,14 +3,19 @@ import './styles.css';
 
 export default ({ children, headers, title, subTitleOne, subTitleTwo }) => (
   <div className='headerContainer'>
-    <div>
+    <div className='headerAd'>
       <div>{ title }</div>
       <div>{ subTitleOne }</div>
       <div>{ subTitleTwo }</div>
     </div>
     <div className='linkDivs'>
       {headers.map((text, idx) =>
-        <a href={`#info${idx + 1}`} className='internalLink'>{ text }</a>
+        <a
+          key={`headerLinks-${idx}`}
+          href={`#info${idx + 1}`}
+          className='internalLink'>
+            { text }
+        </a>
       )}
       { children }
     </div>
